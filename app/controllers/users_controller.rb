f217ef206081e3cd73ce508d298a6068b67d0ee8
wykhuh <wykhuh@users.noreply.github.com>
2015-04-15 05:30:47 -0700

    add flas message for successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # save
+      flash[:success] = "Welcome to sample app"
+      redirect_to @user
     else
       render 'new'
     end
