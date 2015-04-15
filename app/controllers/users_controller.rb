4e5183d8cbf971287a78b42e09b931f927fd6523
wykhuh <wykhuh@users.noreply.github.com>
2015-04-15 04:40:02 -0700

    add strong parameters to signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def show
     @user = User.find(params[:id])
   end
@@ -9,4 +6,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
